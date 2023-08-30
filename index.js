7c0d6231ac49b9060d32b7d5f7243834ba7e49a2
leandro gabriel <leandrogabrieljsuni@gmail.com>
2023-08-30 16:07:23 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const pokedex = [
+    {
+      nome: "bulbassauro",
+      indicie: "0001",
+      tipo: ["gramado", "toxico"]
+    },
+    {
+      nome: "ivysauro",
+      indicie: "0002",
+      tipo: ["gramado", "toxico"]
+    },
+    {
+      nome: "venusauro",
+      indicie: "0003",
+      tipo: ["gramado", "toxico"]
+    },
+    {
+      nome: "chamander",
+      indicie: "0004",
+      tipo: ["fogo"]
+    },
+    {
+      nome: "chameleon",
+      indicie: "0005",
+      tipo: ["fogo"]
+    },
+    {
+      nome: "charizard",
+      indicie: "0006",
+      tipo: ["fogo", "voo"]
+    },
+    {
+      nome: "squirtie",
+      indicie: "0007",
+      tipo: ["agua"]
+    },
+    {
+      nome: "wartorle",
+      indicie: "0008",
+      tipo: ["agua"]
+    },
+    {
+      nome: "blastoise",
+      indicie: "0009",
+      tipo: ["agua"]
+    }
+]
+
+console.log(pokedex)
